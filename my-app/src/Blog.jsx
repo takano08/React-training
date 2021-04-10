73b8f2c9bf553ac918ae4c404200e991e0981f69
takano <ri1saxxminny83xx@gmail.com>
2021-04-10 23:41:31 +0900

    React入門4-2

---
+++ my-app/src/Blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "./Article";
+
+class Blog extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+           <div>
+               <Article />
+           </div>
+        )
+    }
+}
+export default Blog

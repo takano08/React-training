c75ef21ac3099256adadece81fa3f0890ea4c24d
takano <ri1saxxminny83xx@gmail.com>
2021-04-15 00:01:58 +0900

    ライフサイクル2

--- my-app/src/Blog.jsx
+++ my-app/src/Blog.jsx
@@ -13,6 +13,17 @@ class Blog extends React.Component{
         document.getElementById('counter').addEventListener('click',this.countUp)
     }
 
+    componentDidUpdate() {
+       if(this.state.count >= 10) {
+           this.setState({count: 0} )
+       }
+
+    }
+
+    componentWillUnmount() {
+        document.getElementById('counter').removeEventListener('click',this.countUp)
+    }
+
 
     togglePublished = () => {
         this.setState({
--- my-app/src/LikeButton.jsx
+++ my-app/src/LikeButton.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+/**
+ * @param props
+ */
 const LikeButton = (props = props) => {
     return(
         <button id={'counter'}>いいね数: {props.count}</button>

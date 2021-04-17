12286922233d3b79cd05a56948b1dcde311c151a
takano <ri1saxxminny83xx@gmail.com>
2021-04-17 19:21:59 +0900

    React Hooksでstateを扱う

--- my-app/src/Blog.jsx
+++ my-app/src/Blog.jsx
@@ -3,50 +3,42 @@ import Article from "./Article";
 import {Foo,Bar} from './components/FooBar'
 import Hoge from "./components/Hoge";
 
-class Blog extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            isPublished:false,
-            count: 0
-        }
-    }
-    componentDidMount() {
-        document.getElementById('counter').addEventListener('click',this.countUp)
-    }
+const Blog = () => {
 
-    componentDidUpdate() {
-       if(this.state.count >= 10) {
-           this.setState({count: 0} )
-       }
-
-    }
-
-    componentWillUnmount() {
-        document.getElementById('counter').removeEventListener('click',this.countUp)
-    }
+    // componentDidMount() {
+    //     document.getElementById('counter').addEventListener('click',this.countUp)
+    // }
+    //
+    // componentDidUpdate() {
+    //    if(this.state.count >= 10) {
+    //        this.setState({count: 0} )
+    //    }
+    //
+    // }
+    //
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click',this.countUp)
+    // }
 
 
-    togglePublished = () => {
-        this.setState({
-            isPublished:!this.state.isPublished
-            })
-    };
+    // togglePublished = () => {
+    //     this.setState({
+    //         isPublished:!this.state.isPublished
+    //         })
+    // };
 
-    countUp = () => {
-        this.setState({
-            count: this.state.count +1
-        })
-    };
+    // countUp = () => {
+    //     this.setState({
+    //         count: this.state.count +1
+    //     })
+    // };
 
 
-    render() {
+
         return (
            <>
                <Article title={"Reactの使い方"}
-                        isPublished={this.state.isPublished}
-                        toggle={()=> this.togglePublished()}
-                        count={this.state.count}
+                        // count={this.state.count}
                />
                <Foo />
                <Bar />
@@ -54,7 +46,6 @@ class Blog extends React.Component{
            </>
 
         )
-    }
 }
 
 export default Blog;

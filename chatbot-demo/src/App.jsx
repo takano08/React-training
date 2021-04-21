3cf41aada6a2eb5e4f6ea6a004eb234d4f7a5037
takano <ri1saxxminny83xx@gmail.com>
2021-04-22 01:23:05 +0900

    関数コンポーネントの作成

--- chatbot-demo/src/App.jsx
+++ chatbot-demo/src/App.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css'
+import {AnswersList} from "./components/index";
+
 
 export  default class App extends React.Component {
     constructor(props) {
       super(props);
       this.state= {
-        answer:[],
+        answers:[],
         chats:[],
         currentId:"init",
         dataset:defaultDataset,
         open:false
       }
-
   }
 
-  render(){
+  initAnswer = () => {
+        const initDataset =this.state.dataset[this.state.currentId]
+        const initAnswers =initDataset.answers;
+
+        this.setState({
+            answers: initAnswers
+        })
+  }
+
+  componentDidMount() {
+        this.initAnswer()
+  }
+
+    render(){
       return(
           <section className="c-section">
               <div className="c-box">
-                  {this.state.currentId}
+                  <AnswersList answers={this.state.answers} />
               </div>
           </section>
       );
